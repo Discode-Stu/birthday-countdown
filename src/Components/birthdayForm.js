ae46b6afcd73c3cddf51d73bfdfd85858f81d9b6
Discode Stu <dstuart85@yahoo.com>
2020-12-05 10:27:26 -0500

    added generate countdown button

--- src/Components/birthdayForm.js
+++ src/Components/birthdayForm.js
@@ -1,9 +1,10 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import Datepicker, {CalendarContainer} from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css';
 
 const BirthdayForm = () => {
   const [selectedDate, setSelectedDate] = useState(new Date())
+  const [formCompleted, setFormCompleted] = useState(false)
 
   const MyContainer = ({ className, children }) => {
     return (
@@ -17,24 +18,41 @@ const BirthdayForm = () => {
     );
   };
 
-  return ( 
-    <div className="datepicker-container">
-      <Datepicker 
-          selected={selectedDate} 
-          onChange={date => setSelectedDate(date)} 
-          
-          showTimeSelect 
-          showTimeInput
-          timeFormat="HH:mm"
-          timeIntervals={5}
-          timeCaption="time"
-          dateFormat="MMMM d, yyyy h:mm aa"
-          calendarContainer={MyContainer}
-          isClearable
-          placeholderText="What's your birthday?"
+  const handleGenerate = () => {
+    console.log(selectedDate, formCompleted)
+    setFormCompleted(true)
+  }
 
-      />
-    </div> 
+  const RenderStuff = () => {
+    // const birthdate = selectedDate
+    const stuffRendered = formCompleted ? <div> hello </div> : <h2>boop</h2>
+    return stuffRendered
+  }
+
+  return ( 
+    <div>
+      <div className="datepicker-container">
+        <Datepicker 
+            selected={selectedDate} 
+            onChange={date => setSelectedDate(date)} 
+            
+            // showTimeSelect 
+            showTimeInput
+            timeFormat="HH:mm"
+            timeIntervals={5}
+            timeCaption="time"
+            dateFormat="MMMM d, yyyy h:mm aa"
+            calendarContainer={MyContainer}
+            isClearable
+            placeholderText="What's your birthday?"
+
+        />
+      </div> 
+      <a onClick={handleGenerate} >Generate Countdown</a>
+      <RenderStuff />
+
+      
+    </div>
   );
 }
 
