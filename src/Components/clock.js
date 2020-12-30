7adbeb880cd68be983c58a8f8dfdfd59f35222ad
Discode Stu <dstuart85@yahoo.com>
2020-12-29 22:35:59 -0500

    added happy birthday message styles

--- src/Components/clock.js
+++ src/Components/clock.js
@@ -140,7 +140,11 @@ const Clock = (props) => {
     <div className='countdown'>
       {
         timeRemaining === 0 ?
-          <h1>Happy Birthday!!!</h1>
+          <div className='message-container'>
+            <p className="message-container__title">Countdown Complete</p>
+            <p className="message-container__message">HAPPY BIRTHDAY!!</p>
+          </div>
+
         :
         <div>
           <div>
