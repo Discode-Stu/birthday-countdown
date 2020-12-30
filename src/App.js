0bfce7c170d642cb62b4acbf8308d1fe2bb4e512
Discode Stu <dstuart85@yahoo.com>
2020-12-29 20:40:29 -0500

    added header styles

--- src/App.js
+++ src/App.js
@@ -8,8 +8,14 @@ import BirthdayForm from './Components/birthdayForm'
 function App() {
   return ( 
     <div>
-      <h1>Birthday Countdown</h1>
-      <BirthdayForm />
+      <header>
+        <h1>Birthday Countdown</h1>
+        <div className="header__skew">
+          <div className="header__subskew">
+            <BirthdayForm />
+          </div>
+        </div>
+      </header>
     </div> 
   )
 }
